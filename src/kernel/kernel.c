6d6da96f68ed2228033181219cc528ab96a3819f
Cody <cody@codyq.dev>
2023-11-30 11:41:59 -0600

    :3

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -14,6 +14,14 @@
 
 void kernel_main(void) {
   print_clear();
+  for (int r = 0; r < 24; r++) {
+    for (int c = 0; c < 80; c += 3) {
+      int color = (r + c) % 15 + 1;
+      print_set_color(color, PRINT_COLOR_BLACK);
+      printf(":3 ");
+    }
+    printf("%n");
+  }
   print_set_color(PRINT_COLOR_YELLOW, PRINT_COLOR_BLACK);
   printf("printf\n\tint: %d\n\tstring: %s\n\tchar: %c\n\tpercent: "
          "%%\n\tnothing: %n",
